b90be7cffe19f9af6c76b5304ffa4547668260eb
sarwar-asik <shasik2021@gmail.com>
2023-05-04 17:06:59 +0600

    unknown never null

---
+++ src/null.never.unknown.ts
@@ -0,0 +1,30 @@
+const colors = require("colors")
+
+console.log("null ........")
+const SearchName  =(value:string |null)=>{
+    if(value=== null){
+        console.log("it is null")
+    }
+    else{
+        console.log("It is not null")
+    }
+}
+
+SearchName(null)
+
+const CarSpeed =(speeds:unknown)=>{
+
+
+}
+
+
+CarSpeed(20)
+CarSpeed("20 jas ")
+
+
+function throwError(message:string):never{
+
+    throw new Error("Error is mortal")
+}
+
+throwError("New error throw kortesi")
