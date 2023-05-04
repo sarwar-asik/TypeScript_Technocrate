f6123998b67c03ea205a59bd0bf061db581a2a0b
sarwar-asik <shasik2021@gmail.com>
2023-05-04 11:20:05 +0600

    crush type

---
+++ src/allis.ts
@@ -0,0 +1,32 @@
+
+type CrusType={
+    name:string,
+    age?:number,
+    profession:string,
+    address:string,
+    homeAddress?:string
+}
+
+const crush1:CrusType={
+    name:"Tinisa",
+    age:22,
+    profession:"Developer",
+    address:"Dhaka"
+}
+
+const crush2:CrusType ={
+    name:"Pinia",
+    // age:22,
+    profession:"actress",
+    address:"chtg"
+
+}
+
+type CrushMarriedType = boolean ;
+
+const isCrushMarried :CrushMarriedType = false ;
+
+
+type CourseNameType =string ;
+
+const CourseName:CourseNameType ="Next Level web Development"  ;
