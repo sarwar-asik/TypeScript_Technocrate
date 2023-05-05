239228307f7f2172adfd535efef04d276b1b5833
sarwar-asik <shasik2021@gmail.com>
2023-05-05 17:00:25 +0600

    added generic type s

---
+++ src/modules/generic-types.ts
@@ -0,0 +1,38 @@
+
+const rollNumbers:number[] =[22,332,11]
+const rollNumbers2 :Array<string> = ["11","12","44"]
+
+const rollNumbers3 :Array<boolean> =[true,false,true]
+
+const rollAndAge :Array<{name:string,age:number}> =[
+    {name:"Dinki",age:211},
+    {name:"pinki",age:21}
+]
+
+
+// or ///
+
+
+type GenericArray <T> =Array<T>
+
+
+const rollNumber3:GenericArray<number> =[22,332,11]
+const rollNumbers24:GenericArray<string> = ["11","12","44"]
+
+
+type RollandAge5 ={name:string,age:number}
+
+const rollAndAge2 :GenericArray<RollandAge5> =[
+    {name:"Dinki",age:211},
+    {name:"pinki",age:21}
+]
+
+type GenericTurple<X,Y> = [X,Y]
+
+interface salaryTypes{name:string,salary:number}
+
+const relations :GenericTurple<string ,number> =["tinA",33]
+const relationSalary :GenericTurple<salaryTypes,string> =[
+    {name:"KK tong",salary:3000},
+    "Kahrina kaife"
+]
