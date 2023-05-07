39c15cb349763bf780a01d0e1f9e5ea8541fa0fd
sarwar-asik <shasik2021@gmail.com>
2023-05-08 01:18:25 +0600

    get AND set

--- src/module4/access-modify.ts
+++ src/module4/access-modify.ts
@@ -2,14 +2,33 @@ import "colors";
 
 class BankAccount {
   id: number;
-  name: string;
-  balance: number;
+  public name: string;
+  private balance: number;
   constructor(id: number, name: string, balance: number) {
     this.id = id;
     this.name = name;
     this.balance = balance;
   }
+  getBalance(): number {
+    return this.balance;
+  }
+
+  get balance2() {
+    return this.balance;
+  }
+  set deposit(amount: number) {
+    this.balance = this.balance + amount;
+  }
 }
 
+class StudentAccount {}
+
 const myAccount = new BankAccount(444, "Germany", 44);
-console.log(myAccount );
+// myAccount.balance = 0;
+myAccount.getBalance();
+
+// console.log(myAccount);
+console.log(myAccount.getBalance());
+
+myAccount.deposit = 300;
+console.log(myAccount.balance2, "balance");
