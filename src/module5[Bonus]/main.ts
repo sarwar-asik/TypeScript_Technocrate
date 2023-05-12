4527e788dcd6ca533abbfb732485d0f698dc871a
sarwar-asik <shasik2021@gmail.com>
2023-05-12 14:48:02 +0600

    added exports

---
+++ src/module5[Bonus]/main.ts
@@ -0,0 +1,7 @@
+export const add=(mainPara1:string,mainPara2:number)=>{
+    return mainPara1 +mainPara2
+}
+
+const Substract =()=>{
+    return "yes ....."
+}
---
+++ src/module5[Bonus]/module.ts
@@ -0,0 +1,8 @@
+import  {add as Add2} from "./main"
+
+
+const add=(para1:string,para2:number)=>{
+    return para1+para2
+}
+
+Add2("20",30)
